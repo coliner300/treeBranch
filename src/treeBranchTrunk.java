a8b7d368b30b68d596c498c22e4a677347c53400
91085005 <coliner300@gmail.com>
2020-03-09 13:29:10 -0700

    jose is dumb

--- src/treeBranchTrunk.java
+++ src/treeBranchTrunk.java
@@ -5,5 +5,6 @@ public class treeBranchTrunk
         System.out.println("The trees are dying");
         System.out.println("Mr. Beast planted 20,000,000 tress.");
         System.out.println("There are many types of trees");
+        System.out.println("succulents are nice too!");
     }//end main method
 }//end driver class
